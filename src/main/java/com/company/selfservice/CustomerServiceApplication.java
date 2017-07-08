7edc27ecf2ede4be2459b979c3b811d7e2690cf7
puneetkk <puneet.kankane@gmail.com>
2017-07-08 07:48:50 +0530

    Added code for Cross origin
    Added global configuraation for cross origin in web app configuration. It will allow requests from all origins at the moment.

--- src/main/java/com/company/selfservice/CustomerServiceApplication.java
+++ src/main/java/com/company/selfservice/CustomerServiceApplication.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.company.selfservice.services.AddressService;
 import com.company.selfservice.services.CustomerService;
@@ -27,5 +31,18 @@ public class CustomerServiceApplication implements CommandLineRunner {
 		//addressService.createAddress("Number 1", "Queensbury Mansion", "Bournemouth", "UK", "BH12QA");
 		//addressService.createAddress("Number 2", "Some Apartment", "Bournemouth", "UK", "BH12AB");
 		System.out.println(customerService.createCustomer("Puneet", "Kankane", "pk@gmail.com", "Number 1", "Queensbury Mansion", "Bournemouth", "UK", "BH12QA", "pwd"));
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurerAdapter() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/addressservice").allowedOrigins("*");
+	            registry.addMapping("/customerservice").allowedOrigins("*");
+	            registry.addMapping("/userservice").allowedOrigins("*");
+	            registry.addMapping("/**").allowedOrigins("*");
+	        }
+	    };
 	}
 }
